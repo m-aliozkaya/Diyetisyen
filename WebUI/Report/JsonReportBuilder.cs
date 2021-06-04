45055ecab7c3fd5cc7871327b786ae3b49f8171b
beyzahyt <beyzardm502@gmail.com>
2021-06-04 13:35:56 +0300

    report system added

---
+++ WebUI/Report/JsonReportBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUI.Report
+{
+    public class JsonReportBuilder : ReportBuilderBase
+    {
+        public JsonReportBuilder(ReportInfo reportInfo) : base(reportInfo)
+        {
+
+        }
+
+        public override string BuildDietInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string BuildPatientInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebUI/Report/ReportInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebUI.Entity;
+
+namespace WebUI.Report
+{
+    public class ReportInfo
+    {
+        public Patient Patient  { get; set; }
+    }
+}
