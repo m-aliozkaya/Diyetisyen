737e09fc0e50be159b06f42de9ffcdfea09ceda0
Muhammed Ali ÖZKAYA <m_aliozkaya@hotmail.com>
2021-06-04 18:35:11 +0300

    Login page added

--- WebUI/App_Start/RouteConfig.cs
+++ WebUI/App_Start/RouteConfig.cs
@@ -18,6 +18,7 @@ namespace WebUI
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            
         }
     }
 }
---
+++ WebUI/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+
+        public ActionResult Index()
+        {
+            return View();
+        }              
+        
+        [HttpPost]
+        public ActionResult Index(LoginModel model)
+        {
+            return View();
+        }
+
+    }
+}
